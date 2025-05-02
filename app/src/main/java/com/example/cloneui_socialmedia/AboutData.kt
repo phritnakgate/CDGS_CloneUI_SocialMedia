870a11f39c14cdb1c9a7f9ad05e7b90134796fa4
Phrit Nakgate <boss5500216@gmail.com>
2025-05-02 10:19:31 +0700

    feat(profile): profile page remain ripple

---
+++ app/src/main/java/com/example/cloneui_socialmedia/AboutData.kt
@@ -0,0 +1,7 @@
+package com.example.cloneui_socialmedia
+
+data class AboutData(
+    val title: String,
+    val description: String,
+    val icon: Int
+)
--- app/src/main/java/com/example/cloneui_socialmedia/ProfileFragmentActivity.kt
+++ app/src/main/java/com/example/cloneui_socialmedia/ProfileFragmentActivity.kt
@@ -3,12 +3,42 @@ package com.example.cloneui_socialmedia
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.google.android.material.divider.MaterialDivider
 
 class ProfileFragmentActivity : Fragment(R.layout.fragment_profile) {
+
+    //Function to handle color change
+    private fun highlightTab(isPostTab : Boolean) {
+        val postBtnImg = view?.findViewById<AppCompatImageView>(R.id.profile_postBtnImg)
+        val postText = view?.findViewById<TextView>(R.id.profile_postBtnText)
+        val postDiv = view?.findViewById<MaterialDivider>(R.id.profile_divUnderTabPost)
+        val aboutBtnImg = view?.findViewById<AppCompatImageView>(R.id.profile_aboutBtnImg)
+        val aboutText = view?.findViewById<TextView>(R.id.profile_aboutBtnText)
+        val aboutDiv = view?.findViewById<MaterialDivider>(R.id.profile_divUnderTabAbout)
+        if(isPostTab){
+            postText?.setTextColor(ContextCompat.getColor(requireContext(), R.color.secondaryColor))
+            postDiv?.dividerColor = ContextCompat.getColor(requireContext(), R.color.secondaryColor)
+            postBtnImg?.imageTintList = ContextCompat.getColorStateList(requireContext(), R.color.secondaryColor)
+            aboutText?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            aboutDiv?.dividerColor = ContextCompat.getColor(requireContext(), R.color.disabledColorA30)
+            aboutBtnImg?.imageTintList = ContextCompat.getColorStateList(requireContext(), R.color.black)
+        }else{
+            postText?.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            postDiv?.dividerColor = ContextCompat.getColor(requireContext(), R.color.disabledColorA30)
+            postBtnImg?.imageTintList = ContextCompat.getColorStateList(requireContext(), R.color.black)
+            aboutText?.setTextColor(ContextCompat.getColor(requireContext(), R.color.secondaryColor))
+            aboutDiv?.dividerColor = ContextCompat.getColor(requireContext(), R.color.secondaryColor)
+            aboutBtnImg?.imageTintList = ContextCompat.getColorStateList(requireContext(), R.color.secondaryColor)
+        }
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -38,11 +68,17 @@ class ProfileFragmentActivity : Fragment(R.layout.fragment_profile) {
         }
         val postBtn = view.findViewById<LinearLayout>(R.id.profile_postBtn)
         postBtn.setOnClickListener {
-
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.profile_postAboutFragment, PostsFragmentActivity())
+                .commit()
+            highlightTab(true)
         }
         val aboutBtn = view.findViewById<LinearLayout>(R.id.profile_aboutBtn)
         aboutBtn.setOnClickListener {
-
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.profile_postAboutFragment, AboutFragmentActivity())
+                .commit()
+            highlightTab(false)
         }
     }
 }
