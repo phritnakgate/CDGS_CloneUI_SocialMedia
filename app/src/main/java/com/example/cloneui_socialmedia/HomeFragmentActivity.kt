425263e9d920a9d7ebff3342769df759be5709a8
Phrit Nakgate <boss5500216@gmail.com>
2025-05-06 12:33:26 +0700

    feat(home): posts view

--- app/src/main/java/com/example/cloneui_socialmedia/HomeFragmentActivity.kt
+++ app/src/main/java/com/example/cloneui_socialmedia/HomeFragmentActivity.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.graphics.LinearGradient
 import android.graphics.Shader
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -45,6 +46,17 @@ class HomeFragmentActivity : Fragment(R.layout.fragment_home) {
         storyRecyclerView.adapter = storyAdapter
         storyRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
 
+        //Post RecyclerView
+        val postRecyclerView = view.findViewById<RecyclerView>(R.id.home_posts)
+        val postList = mutableListOf(
+            PostData("John Doe", "johnDoe123", "https://miro.medium.com/v2/resize:fit:920/1*R5zt6Upx-ba905xLU6HNXw.gif", "2025-05-01", "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2456740/header.jpg?t=1744688805", "อยากเล่นเกมนี้มากๆ inZOI พัฒนาโดย inZOI Studio และจัดจำหน่ายโดยคราฟตันเกมนี้จะวางจำหน่ายในวันที่ 28 มีนาคม ค.ศ. 2025", 0, false, 0, 0, 0, false),
+            PostData("Major Group", "majorcineplex", "https://scontent.fbkk14-1.fna.fbcdn.net/v/t39.30808-6/492548952_1133013508855724_70950623530622578_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=bsw3XYm76vEQ7kNvwGtFIfU&_nc_oc=AdmGKf6ulbb44CkY2-ka6J7jEzwGifKfjnjzLgxeQ6N27XtAfnUTPJFIVl1hOgYZRhY&_nc_zt=23&_nc_ht=scontent.fbkk14-1.fna&_nc_gid=0_iid83fTRX8kWdVnuEdBg&oh=00_AfKB3J2tgi1LDCJ-E7vPf3qisi5HjiV1zbOP5h97XmpDWQ&oe=681F5318", "2025-03-29", "https://scontent.fbkk10-1.fna.fbcdn.net/v/t39.30808-6/492233042_1130141512476257_4684539077288037862_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ymgOphIECdIQ7kNvwGU1OjM&_nc_oc=Adl4a2AqcywfR64Gc4hOm37ksuznln3dpb5GqNQUVkd1Tt5qrxr6OP6Dt5T8Ad0FYfY&_nc_zt=23&_nc_ht=scontent.fbkk10-1.fna&_nc_gid=iRJZlkF6jC34C3_4Ik1_Jw&oh=00_AfLjQz7n8P0G7Er5UAwW6XEcPGv0-G4BtuiEpYwyL4cgyg&oe=681F341C", "จากเกมชื่อดังสู่ภาพยนตร์เหนือจินตนาการ A #Minecraft Movie - ไมน์คราฟต์ มูฟวี่ เข้าฉาย 3 เมษายนนี้ที่ เมเจอร์ ซีนีเพล็กซ์ \n #MinecraftMovie", 0, false, 0, 0, 0, false),
+            PostData("Thailand Game Show", "tgs", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLi16IJRN_HwEwADqGWBdmrRJTUdUfDFYvkw&s", "2025-02-20", "https://www.thailandgameshow.com/wp-content/uploads/2025/02/GAxTGS-2025-Banner-1640px-x-720px.png", "Thailand Game Show ปีนี้จะยิ่งใหญ่กว่าเดิม เมื่อ gamescom asia งานเกมสุดยิ่งใหญ่ในระดับภูมิภาคเอเชีย ย้ายฐานการจัดงานจากสิงคโปร์มาจัดในไทยเป็นครั้งแรกร่วมกับ TGS เกิดเป็น gamescom asia x Thailand Game Show ระหว่างวันที่ 16 - 19 ตุลาคมนี้ ที่ศูนย์การประชุมแห่งชาติสิริกิติ์ ! #thailandgameshow", 0, false, 0, 0, 0, false),
+        )
+        val postAdapter = PostAdapter(postList)
+        postRecyclerView.adapter = postAdapter
+        postRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        postRecyclerView.addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
 
         ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.home_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
---
+++ app/src/main/java/com/example/cloneui_socialmedia/PostData.kt
@@ -0,0 +1,18 @@
+package com.example.cloneui_socialmedia
+
+import java.util.Date
+
+data class PostData (
+    val username : String,
+    val userId : String,
+    val profileImgUrl : String,
+    val datePosted : String,
+    val postImage : String,
+    val postDescription : String,
+    var postLikes : Int,
+    var postLiked : Boolean,
+    val postComments : Int,
+    val postShares : Int,
+    var postBookmark : Int,
+    var postBookmarked : Boolean,
+)
